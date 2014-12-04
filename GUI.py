92205515bdf2e2ac9a3cabe4e8a119e0be2a10be
Zwelf <reeggister@gmail.com>
2014-12-04 22:09:17 +0100

    Fehlerkorrektur
    von Julius übernommen

--- GUI.py
+++ GUI.py
@@ -33,42 +33,42 @@ class GUI:
 
     def show_window(self):
         """
-        Diese Funktion Zeigt (pack) das Fenster an.
+        Diese Funktion zeigt (pack) das Fenster an.
         """
         self.__root.tk.pack()
         
     def quit_window(self):
         """
-        Diese Fenster entfert wieder das Programm.
+        Dieses Fenster entfert wieder das Programm.
         """
         self.__root.tk.unpack()
     
     #MAIN MENU
     def create_mainmenu(self):
         """
-        Diese Funktion erstellt die Elemente des Hauptmenues
+        Diese Funktion erstellt die Elemente des Hauptmenues.
         Voraussetzung: Fenster muss erstellt sein.
         """
         pass
     
     def show_mainmenu(self):
         """
-        Diese Funktion zeigt die Elemente des Hauptmenues an
-        Voraussetzung: Das ertellen [create_mainmenu()] muss erfolgt sein
+        Diese Funktion zeigt die Elemente des Hauptmenues an.
+        Voraussetzung: Das Erstellen [create_mainmenu()] muss erfolgt sein.
         """
         pass
     
     def hide_mainmenu(self):
         """
-        Diese Funktion versteckt die Elemente des Hauptmenue
-        Voraussetzung: Das ertellen muss erfolgt sein
+        Diese Funktion versteckt die Elemente des Hauptmenues.
+        Voraussetzung: Das ertellen muss erfolgt sein.
         """
         pass
     
     #VOCABLE MENU
     def create_vocmenu(self):
         """
-        Diese Funktion erstellt die Elemente des Menue, wo die ganzen Vokabeln angezeigt werden
+        Diese Funktion erstellt die Elemente des Menue, wo die ganzen Vokabeln angezeigt werden.
         Voraussetzung: Das Fenster muss erstellt sein.
         """
         pass
@@ -76,36 +76,36 @@ class GUI:
     def show_vocmenu(self):
         """
         Diese Funktion zeigt die Elemente des Hauptmenues an.
-        Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()]
+        Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()].
         """
         pass
     
     def hide_vocmenu(self):
         """
         Diese Funktion versteckt die Elemente des Hauptmenues.
-        Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()]
+        Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()].
         """
         pass
 
     #EXIT MENU
     def create_exitmenu(self):
         """
-        Diese Funktion erstellt das Exit-Menue (mit Elementen)
+        Diese Funktion erstellt das Exit-Menue (mit Elementen).
         Voraussetzung: Das Fenster muss erstellt sein.
         """
         pass
 
     def show_exitmenu(self):
         """
-        Diese Funktion zeigt das Exit-Menue
-        Voraussetzung: Menue muss existieren
+        Diese Funktion zeigt das Exit-Menue.
+        Voraussetzung: Menue muss existieren.
         """
         pass
     
     def hide_exitmenu(self):
         """
-        Diese Funktion versteckt das Exit-Menue
-        Voraussetzung: Menue muss existieren
+        Diese Funktion versteckt das Exit-Menue.
+        Voraussetzung: Menue muss existieren.
         """
         pass
     
