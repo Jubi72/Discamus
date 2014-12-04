e7b8be46f7f1cf9dc1037a909305a8e28d188b8d
Zwelf <reeggister@gmail.com>
2014-12-04 09:13:54 +0100

    erste Funktionen für die GUI erstellt
    Jetzt gibt es Beispiele, wie die Funktionen der GUI aussehen sollen.
    Daher müssen nur noch die Funktionen ausgeuellt und neue erstellt
    werden.

--- GUI.py
+++ GUI.py
@@ -1,1 +1,61 @@
 from interface import *
+
+def create_window():
+    """
+    Das Fenster wird Erstellt (ohne inhalt, aber mit den ganzen Eigenschaften)
+    """
+    pass
+
+def show_window():
+    """
+    Diese Funktion Zeigt (pack) das Fenster an.
+    """
+    pass
+    
+def quit_window():
+    """
+    Diese Fenster entfert wieder das Programm.
+    """
+    pass
+
+def create_mainmenu():
+    """
+    Diese Funktion erstellt die Elemente des Hauptmenues
+    Voraussetzung: Fenster muss erstellt sein.
+    """
+    pass
+
+def show_mainmenu():
+    """
+    Diese Funktion zeigt die Elemente des Hauptmenues an
+    Voraussetzung: Das ertellen [create_mainmenu()] muss erfolgt sein
+    """
+    pass
+
+def hide_mainmenu():
+    """
+    Diese Funktion versteckt die Elemente des Hauptmenue
+    Voraussetzung: Das ertellen muss erfolgt sein
+    """
+    pass
+
+def create_vocmenu():
+    """
+    Diese Funktion erstellt die Elemente des Menue, wo die ganzen Vokabeln angezeigt werden
+    Voraussetzung: Das Fenster muss erstellt sein.
+    """
+    pass
+
+def show_vocmenu():
+    """
+    Diese Funktion zeigt die Elemente des Hauptmenues an.
+    Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()]
+    """
+    pass
+
+def hide_vocmenu():
+    """
+    Diese Funktion versteckt die Elemente des Hauptmenues.
+    Voraussetzung: Erstellen muss erfolgt sein [create_vocmenu()]
+    """
+    pass
