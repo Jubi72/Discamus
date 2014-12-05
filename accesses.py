95d071cb5421247c14e0ab37deb1ecd048ef4d41
Zwelf <reeggister@gmail.com>
2014-12-05 12:17:34 +0100

    in class geändert
    um die Zugriffe einfacher zu machen, in Klasse geschrieben

--- accesses.py
+++ accesses.py
@@ -1,42 +1,42 @@
 import os
-
-def pruefe_dateipfade():
-    """
-    Diese Funktion prueft, ob alle noetigen Dateipfade existieren, wenn nicht, werden diese hier erstllt
-    noetige Dateipfade:
-    %APPDATA%\Rena\
-    %APPDATA%\Rena\stapel
-    %APPDATA%\Rena\einstellungen
-    """
-    pass
-
-def decks_list():
-    """
-    Funkion liesst alle Kartenstapel aus %APPDATA%\[PROGRAMMNAME]\stapel
-    Die Stapel sind Dateien, dessen Dateinamen so aussen: *.rna
-    """
-    #return liste
-    pass
-
-def deck_info(nameofdeck):
-    """
-    Diese Funktion gibt von einem Stapel die Infos zurueck
-    return [name, letzterAufruf, AnzahlKarten, Lernstand]
-    """
-    #return infos
-    pass
-
-def deck_cards():
-    """
-    Diese Funktion gibt aus der Stapeldatei die Karten als Liste/Dictionary zurueck
-    Aufbau der Datei:
-    + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-    | Name|letzterAufruf|AnzahlKarten|Lernstand[0-100] ::infos ueber den Stapel |
-    | Vorderseite|Rueckseite|Einseiteig(0)/Zweiseitig(1)|lernstand ::Kartenifos |
-    | ...                                                                       |
-    + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-    """
-    pass
-
-
-
+class funktion:
+    def pruefe_dateipfade(self):
+        """
+        Diese Funktion prueft, ob alle noetigen Dateipfade existieren, wenn nicht, werden diese hier erstllt
+        noetige Dateipfade:
+        %APPDATA%\Rena\
+        %APPDATA%\Rena\stapel
+        %APPDATA%\Rena\einstellungen
+        """
+        pass
+    
+    def decks_list(self):
+        """
+        Funkion liesst alle Kartenstapel aus %APPDATA%\[PROGRAMMNAME]\stapel
+        Die Stapel sind Dateien, dessen Dateinamen so aussen: *.rna
+        """
+        #return liste
+        pass
+    
+    def deck_info(self, nameofdeck):
+        """
+        Diese Funktion gibt von einem Stapel die Infos zurueck
+        return [name, letzterAufruf, AnzahlKarten, Lernstand]
+        """
+        #return infos
+        pass
+    
+    def deck_cards(self):
+        """
+        Diese Funktion gibt aus der Stapeldatei die Karten als Liste/Dictionary zurueck
+        Aufbau der Datei:
+        + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+        | Name|letzterAufruf|AnzahlKarten|Lernstand[0-100] ::infos ueber den Stapel |
+        | Vorderseite|Rueckseite|Einseiteig(0)/Zweiseitig(1)|lernstand ::Kartenifos |
+        | ...                                                                       |
+        + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+        """
+        pass
+    
+    
+    
