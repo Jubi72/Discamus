003c887cf3221990fe3d1d3be3cdd798aec1ad80
Zwelf <reeggister@gmail.com>
2014-12-15 10:28:24 +0100

    privatisierung einzeler Funktionen und Erklärung hinzugefügt
    Erklärung hinzugefügt

--- src/access.py
+++ src/access.py
@@ -1,6 +1,24 @@
 import os
 import time
 
+"""
+ + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+ | Anleitung, fuer die Nutzung der Funktionen der Klasse                                         |
+ | Autor: Manuel                                                                                 |
+ + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+ | deck_list() :: gibt alle Decks zurueck                                                        |
+ | deck_list_info() :: gibt alle Decks mit den Infos zurueck                                     |
+ | deck_create(str:name, str:kategorie, str:description) :: erstellt ein Kartenstapel            |
+ | deck_delete(str:dateiname) :: loescht ein Kartenstapel                                        |
+ | deck_load(str:dateiname) :: Laden eines Kartenstapels, notwendig um dieses zu nutzen          |
+ |  deck_info() :: gibt die Infos des aktuellen Kartenstapels zurueck                            |
+ |  card_create(str:seite1, str:seite2) :: fuegt zum aktuellen Kartestapel eine Karte hinzu      |
+ |  card_delete(str:id) :: loescht eine Karte                                                    |
+ |  random_card() :: gibt eine zufaellige Karte aus dem geladenen Deck zurueck und loescht diese |
+ |  last_card() :: gibt die zuletzt ausgegebene Karte zurueck                                    |
+ + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+"""
+
 # -*- coding: utf-8 -*-
 class funktion:
     def __init__(self):
@@ -28,7 +46,7 @@ class funktion:
         self.__card_suffix = ".rna"
         
     #Grundfunktionen, noetig fuer das Programm:
-    def str_valid(self, String, max_len=0):
+    def __str_valid(self, String, max_len=0):
         """
         Diese Funktion prueft, ob der uebergebene String nur Zahlen, Buchstaben (gross, klein),
         einige spezielle (deutsche) sonderzeichen enthaelt, und ob die laenge des Strings stimmt.
@@ -41,27 +59,7 @@ class funktion:
                 return False
         return True
     
-    #Funktionen fuer mehrere Kartenstapel
-    def deck_list(self):
-        """
-        Diese Funktion gibt die Liste aller Kartenstapel zurueck
-        Voraussetzung: falls schon laenger nicht getan: deck_list_update noetig.
-        """      
-        return self.__deck_list
-    
-    def deck_list_info(self):
-        """
-        Diese Funktion gibt die Liste aller Kartenstapel mit Infos zurueck
-        Keine Voraussetzungen
-        Liste: [[dateiname, name, fortschritt, ...],[]
-        """
-        self.deck_list_update()
-        self.__deck_list_info=[]
-        for deck in self.__deck_list:
-            self.__deck_list_info.append([deck]+self.deck_load_info(deck))
-        return self.__deck_list_info
-    
-    def deck_list_update(self):
+    def __deck_list_update(self):
         """
         Diese Funktion liesst alle Kartenstapel aus und schreibt sie in die Variable self.deck_list
         """
@@ -70,8 +68,6 @@ class funktion:
             if len(datei)>4 and datei[-len(self.__card_suffix):-1]+datei[-1]==self.__card_suffix:
                 self.__deck_list.append(datei)
  
- 
-    #Funktionen fuer jeweils ein Kartenstapel
     def __einrueckenZahl(self, zahl, laenge):
         """
         Diese Funktion schreibt in einem String vor die eingegebene Zahl so viele Nullen,
@@ -80,7 +76,7 @@ class funktion:
         anzahlLeerzeichen = laenge-len(str(zahl))
         return max(0, anzahlLeerzeichen)*"0"+str(zahl)
     
-    def timestamp(self):
+    def __timestamp(self):
         lk = time.localtime()
         timestamp  = self.__einrueckenZahl(lk[0],4) #Jahre (laenge 4)
         timestamp += self.__einrueckenZahl(lk[1],2) #Monate (laenge 2)
@@ -89,6 +85,25 @@ class funktion:
         timestamp += self.__einrueckenZahl(lk[4],2) #Minuten (laenge 2)
         timestamp += self.__einrueckenZahl(lk[5],2) #Sekunden (laenge 2)
         return timestamp
+
+    def deck_list(self):
+        """
+        Diese Funktion gibt die Liste aller Kartenstapel zurueck
+        """
+        self.__deck_list_update()      
+        return self.__deck_list
+    
+    def deck_list_info(self):
+        """
+        Diese Funktion gibt die Liste aller Kartenstapel mit Infos zurueck
+        Keine Voraussetzungen
+        Liste: [[dateiname, name, fortschritt, ...],[]
+        """
+        self.__deck_list_update()
+        self.__deck_list_info=[]
+        for deck in self.__deck_list:
+            self.__deck_list_info.append([deck]+self.__deck_load_info(deck))
+        return self.__deck_list_info    
         
     def deck_create(self, name, kategorie, description):
         """
@@ -97,31 +112,32 @@ class funktion:
         name|timestamp|kategorie|beschreibung
         und updatet die Deck-Liste
         """
-        self.deck_list_update()
+        self.__deck_list_update()
         dateiname = name+self.__card_suffix
+        #Erstellen des Dateinamens
         if dateiname in self.__deck_list:
             i=2
             dateiname = name+"_"+str(i)+self.__card_suffix in self.__deck_list
             while dateiname in self.__deck_list:
                 i+=1
                 dateiname = name+"_"+str(i)+self.__card_suffix in self.__deck_list
+        #Schreiben in die Datei
         datei = os.open(self.__deck_dir+dateiname, "r")
-        timestamp =self.timestamp()
+        timestamp =self.__timestamp()
         datei.write(name+"|"+kategorie+"|"+timestamp+"|"+kategorie+"|"+description)
         datei.close()
-        self.deck_list_update()
+        self.__deck_list_update()
     
     def deck_delete(self, dateiname):
         """
         Diese Funktion loescht das angegebene Deck und updatet die Deck-Liste
         """
-        self.deck_list_update()
+        self.__deck_list_update()
         if dateiname in self.__deck_list:
             os.remove(self.__deck_dir+dateiname)
-        self.deck_list_update()
+        self.__deck_list_update()
     
-    def deck_load_info(self, dateiname):
-        #diese Funktion nur beim nicht_laden verwenden
+    def __deck_load_info(self, dateiname):
         """
         Diese Funktion laed nur die Infos, 
         damit beim Auflisten der Dateien nur diese Funktion aufgerufen werden muss.
@@ -148,7 +164,7 @@ class funktion:
         + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
         """
         self.__deck=dateiname
-        self.deck_load_info(dateiname)
+        self.__deck_load_info(dateiname)
         #...
         pass
     
@@ -158,7 +174,6 @@ class funktion:
         Voraussetzung: Deck muss geldaden sein (deck_load())
         -Speichert es in zwei variablen fuer random_card()
         """
-        
         return self.__deck_cards
     
     def load_deck_cards(self):
